6dd521da3f7e0ce5b2054cdffea8f1d7bf59b702
Alex Ch <churkindev@gmail.com>
2021-12-21 14:04:50 +0300

    main

---
+++ Task_211221/TList.h
@@ -0,0 +1,381 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include <string>
+
+/* TListException - класс исключения для TList */
+
+class TListException
+{
+private:
+	std::string desc;
+
+public:
+	TListException(std::string _desc)
+	{
+		desc = _desc;
+	}
+	TListException(const TListException& e)
+	{
+		desc = e.desc;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const TListException& e)
+	{
+		os << "Exception message: " << e.desc << '\n';
+		return os;
+	}
+};
+
+/* TNode - звено списка */
+
+template <class T>
+struct TNode
+{
+	T value;
+	TNode* pNext;
+};
+
+/* TList - односвязный список */
+
+template <class T>
+class TList
+{
+private:
+	TNode<T>* pFirst;
+	int size = 0;
+
+public:
+	TList();
+	TList(const TList<T>& other);
+	~TList();
+
+	TList<T>& operator=(const TList<T>& other);
+
+	bool IsEmpty() const;
+	bool IsNotEmpty() const;
+
+	bool operator==(const TList<T>& other) const;
+	bool operator!=(const TList<T>& other) const;
+
+	void PushFront(T element);
+	void PushBack(T element);
+
+	void PushAfterMax(T element);
+
+	T PopFront();
+	T PopBack();
+	T& Get(int index);
+	T& operator[](int index);
+
+	T GetFirst() const;
+	T GetLast() const;
+
+	void Clear();
+
+	friend std::ostream& operator<<(
+		std::ostream& out,
+		const TList<T>& l)
+	{
+		TNode<T>* node = l.pFirst;
+
+		out << "[ ";
+		while (node != nullptr)
+		{
+			out << node->value << ' ';
+			node = node->pNext;
+		}
+		out << "]";
+
+		return out;
+	}
+
+private:
+	void CopyNodesFrom(const TList<T>& other);
+};
+
+template <class T>
+TList<T>::TList() : pFirst(nullptr) {}
+
+template <class T>
+TList<T>::TList(const TList<T>& other)
+{
+	pFirst = nullptr;
+	CopyNodesFrom(other);
+	size = other.size;
+}
+
+template <class T>
+TList<T>::~TList()
+{
+	Clear();
+}
+
+template <class T>
+TList<T>& TList<T>::operator=(const TList<T>& other)
+{
+	if (this == &other)
+		return *this;
+
+	Clear();
+	CopyNodesFrom(other);
+	size = other.size;
+}
+
+template <class T>
+bool TList<T>::IsEmpty() const
+{
+	return pFirst == nullptr;
+}
+
+template <class T>
+bool TList<T>::IsNotEmpty() const
+{
+	return pFirst != nullptr;
+}
+
+template <class T>
+bool TList<T>::operator==(const TList<T>& other) const
+{
+	TNode<T>* node = pFirst;
+	TNode<T>* nodeOther = other.pFirst;
+
+	//Сравнение элементов звеньев
+	while (node != nullptr && nodeOther != nullptr)
+	{
+		if (node->value != nodeOther->value) return false;
+		node = node->pNext;
+		nodeOther = nodeOther->pNext;
+	}
+
+	//Если в списках различно число элементов -
+	//они не равны
+	if (node != nullptr || nodeOther != nullptr)
+		return false;
+
+	return true;
+}
+
+template <class T>
+bool TList<T>::operator!=(const TList<T>& other) const
+{
+	return !operator==(other);
+}
+
+template <class T>
+void TList<T>::PushFront(T element)
+{
+	TNode<T>* newNode = new TNode<T>();
+	newNode->value = element;
+	newNode->pNext = pFirst;
+	pFirst = newNode;
+
+	size++;
+}
+
+template <class T>
+void TList<T>::PushBack(T element)
+{
+	TNode<T>* newNode = new TNode<T>();
+	newNode->value = element;
+	newNode->pNext = nullptr;
+
+	//Если список пуст, новое звено - первое
+	if (IsEmpty())
+		pFirst = newNode;
+	//Иначе добавим его в конец списка
+	else
+	{
+		TNode<T>* node = pFirst;
+		while (node->pNext != nullptr)
+			node = node->pNext;
+
+		node->pNext = newNode;
+	}
+	size++;
+}
+
+template <class T>
+void TList<T>::PushAfterMax(T element)
+{
+	TNode<T>* newNode = new TNode<T>();
+	newNode->value = element;
+	newNode->pNext = nullptr;
+
+	//Если список пуст, новое звено - первое
+	if (IsEmpty())
+		pFirst = newNode;
+	//Иначе
+	else
+	{
+		//1. Найдём звено с макс. значением
+		TNode<T>* nodeMax = pFirst;
+
+		TNode<T>* node = pFirst;
+		while (node->pNext != nullptr)
+		{
+			if (node->value > nodeMax->value)
+				nodeMax = node;
+
+			node = node->pNext;
+		}
+		//Последняя проверка также необходима
+		if (node->value > nodeMax->value)
+			nodeMax = node;
+
+		//2. Добавим после него новое звено
+		TNode<T>* temp = nodeMax->pNext;
+		nodeMax->pNext = newNode;
+		newNode->pNext = temp;
+	}
+	size++;
+}
+
+template <class T>
+T TList<T>::PopFront()
+{
+	if (IsEmpty())
+		throw TListException(
+			"Can't pop front from an empty list");
+
+	T value = pFirst->value;
+	TNode<T>* oldFirst = pFirst;
+	pFirst = oldFirst->pNext;
+	delete oldFirst;
+
+	size--;
+	return value;
+}
+
+template <class T>
+T TList<T>::PopBack()
+{
+	if (IsEmpty())
+		throw TListException(
+			"Can't pop back from an empty list");
+
+	TNode<T>* prevNode = pFirst;
+	TNode<T>* node = pFirst;
+
+	while (node->pNext != nullptr)
+	{
+		prevNode = node;
+		node = node->pNext;
+	}
+
+	T value = node->value;
+
+	//В списке был 1 элемент - pFirst
+	if (prevNode == node)
+	{
+		delete pFirst;
+		pFirst = nullptr;
+	}
+	//В списке было более 1 элемента
+	else
+	{
+		delete node;
+		prevNode->pNext = nullptr;
+	}
+
+	size--;
+	return value;
+}
+
+template <class T>
+T& TList<T>::Get(int index)
+{
+	if (index < 0 || index >= size)
+		throw TListException("Index out of bounds (list size is "
+			+ std::to_string(size) + ")");
+
+	int i = 0;
+	TNode<T>* node = pFirst;
+	while (node != nullptr)
+	{
+		if (i == index)
+			return node->value;
+
+		node = node->pNext;
+		i++;
+	}
+}
+
+template <class T>
+T& TList<T>::operator[](int index)
+{
+	return Get(index);
+}
+
+template <class T>
+T TList<T>::GetFirst() const
+{
+	if (IsEmpty())
+		throw TListException(
+			"Can't get first element of an empty list");
+	return pFirst->value;
+}
+
+template <class T>
+T TList<T>::GetLast() const
+{
+	if (IsEmpty())
+		throw TListException(
+			"Can't get last element of an empty list");
+
+	TNode<T>* node = pFirst;
+	while (node->pNext != nullptr)
+		node = node->pNext;
+
+	return node->value;
+}
+
+template <class T>
+void TList<T>::Clear()
+{
+	TNode<T>* node;
+	while (pFirst != nullptr)
+	{
+		node = pFirst;
+		pFirst = pFirst->pNext;
+		delete node;
+	}
+}
+
+/* Последовательное копирование звеньев,
+   начиная с 1-го */
+template <class T>
+void TList<T>::CopyNodesFrom(
+	const TList<T>& other
+)
+{
+	if (other.IsNotEmpty())
+	{
+		TNode<T>* newNode;
+		//Копирование первого звена
+		newNode = new TNode<T>;
+		newNode->value = other.pFirst->value;
+		newNode->pNext = nullptr;
+		pFirst = newNode;
+
+		//Копирование остальных звеньев:
+
+		//Предыдущее звено текущего списка
+		TNode<T>* prevNewNode = newNode;
+
+		//Звено "другого" списка
+		TNode<T>* otherNode = other.pFirst->pNext;
+
+		while (otherNode != nullptr)
+		{
+			newNode = new TNode<T>;
+			newNode->value = otherNode->value;
+			newNode->pNext = nullptr;
+
+			prevNewNode->pNext = newNode;
+
+			prevNewNode = newNode;
+			otherNode = otherNode->pNext;
+		}
+	}
+}
---
+++ Task_211221/Task_211221.cpp
@@ -0,0 +1,151 @@
+﻿#include <iostream>
+#include "TList.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "TList demo\n";
+    cout << "==================\n";
+
+    TList<int> list;
+
+    cout << "list:\n" << list << "\n\n";
+
+
+    cout << "Pushing back:\n";
+    for (int i = 1; i <= 5; i++)
+    {
+        list.PushBack(i);
+        cout << list << "\n";
+    }
+
+    cout << "\n";
+
+    cout << "Pushing front:\n";
+    for (int i = 6; i <= 10; i++)
+    {
+        list.PushFront(i);
+        cout << list << "\n";
+    }
+
+    cout << "\n";
+
+    TList<int> list2 = list;
+
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "Popped front: " << list.PopFront() << "\n";
+        cout << list << "\n\n";
+    }
+
+    TList<int> list3(list);
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "Popped back: " << list.PopBack() << "\n";
+        cout << list << "\n\n";
+    }
+
+    cout << "Let's try to pop back/front from \"list\" again:\n";
+
+    try
+    {
+        list.PopFront();
+    }
+    catch (TListException e)
+    {
+        cout << e;
+    }
+
+    try
+    {
+        list.PopBack();
+    }
+    catch (TListException e)
+    {
+        cout << e << '\n';
+    }
+
+    cout << "We copied this lists from some previous states of \"list\":\n\n";
+    cout << "list2:\n" << list2 << "\n\n";
+    cout << "list3:\n" << list3 << "\n\n";
+
+    cout << "These 2 lists are not equal:\n";
+    cout << "(list2 == list3) = " << (list2 == list3) << '\n';
+
+    cout << "But their last elements are equal:\n";
+    cout << "list2.GetLast() = " << list2.GetLast() << "\n";
+    cout << "list3.GetLast() = " << list3.GetLast() << "\n\n";
+
+    cout << "We can make this lists equal with list2.PopFront():\n";
+    for (int i = 1; i <= 5; i++)
+    {
+        list2.PopFront();
+        cout << list2 << "\n\n";
+    }
+
+    cout << "list2:\n" << list2 << "\n\n";
+    cout << "list3:\n" << list3 << "\n\n";
+
+    cout << "(list2 == list3) = " << (list2 == list3) << "\n";
+
+    cout << "list2[3] = " << list2[3] << "\n\n";
+
+    cout << "Trying to get list2[20]:\n";
+    try
+    {
+        int k = list2[20];
+    }
+    catch (TListException e)
+    {
+        cout << e << "\n\n";
+    }
+
+    /* ........ Task (21.12.21) ........ */
+
+    cout << "Task (21.12.21):\n";
+    cout << "===============\n";
+
+    cout << "Test 1: \n\n";
+
+    TList<int> l1;
+
+    l1.PushBack(1);
+    l1.PushBack(3);
+    l1.PushBack(2);
+    l1.PushBack(4);
+    l1.PushBack(2);
+
+    cout << l1 << "\n";
+    l1.PushAfterMax(1024);
+    cout << l1 << "\n\n";
+
+    cout << "Test 2: \n\n";
+
+    TList<int> l2;
+
+    l2.PushBack(1);
+    l2.PushBack(3);
+    l2.PushBack(2);
+    l2.PushBack(11);
+    l2.PushBack(120);
+
+    cout << l2 << "\n";
+    l2.PushAfterMax(1024);
+    cout << l2 << "\n\n";
+
+    cout << "Test 3: \n\n";
+
+    TList<int> l3;
+
+    l3.PushBack(4);
+    l3.PushBack(3);
+    l3.PushBack(2);
+    l3.PushBack(1);
+
+    cout << l3 << "\n";
+    l3.PushAfterMax(1024);
+    cout << l3 << "\n";
+}
