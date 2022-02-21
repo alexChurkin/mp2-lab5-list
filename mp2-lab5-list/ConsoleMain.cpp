b08dab78968f92262ee32ce22f9deeb18c1d82bf
alexChurkin <churkindev@gmail.com>
2022-02-21 22:49:56 +0300

    TList fixes; iterator added

--- mp2-lab5-list/ConsoleMain.cpp
+++ mp2-lab5-list/ConsoleMain.cpp
@@ -7,5 +7,88 @@ int main()
 {
 	cout << "Console TList demo\n";
 	cout << "==================\n";
+	TList<int> l;
+	l.InsFirst(3);
+	l.InsFirst(2);
+	l.InsFirst(1);
 
+	//1 2 3
+	cout << l << "\n";
+
+	l.InsLast(4);
+
+	//1 2 3 4
+	for (l.Revert(); !l.IsEnd(); l.GoNext())
+	{
+		cout << l.GetCurr() << ' ';
+	}
+	cout << "\n";
+
+	l.Revert();
+	l.InsCurr(0);
+
+	//0 1 2 3 4
+	for (l.Revert(); !l.IsEnd(); l.GoNext())
+	{
+		cout << l.GetCurr() << ' ';
+	}
+	cout << "\n";
+
+	l.InsCurr(5);
+
+	//0 1 2 3 4 5
+	for (l.Revert(); !l.IsEnd(); l.GoNext())
+	{
+		cout << l.GetCurr() << ' ';
+	}
+	cout << "\n";
+
+	l.Revert();
+	l.GoNext();
+	l.InsCurr(777);
+
+	//0 777 1 2 3 4 5
+	for (l.Revert(); !l.IsEnd(); l.GoNext())
+	{
+		cout << l.GetCurr() << ' ';
+	}
+	cout << "\n";
+
+	l.DelFirst();
+
+	//777 1 2 3 4 5
+	for (l.Revert(); !l.IsEnd(); l.GoNext())
+	{
+		cout << l.GetCurr() << ' ';
+	}
+	cout << "\n";
+
+	//Trying to delete current when it's == end
+	try
+	{
+		l.DelCurr();
+	}
+	catch (TLException e)
+	{
+		cout << e;
+	}
+
+	l.Revert();
+	l.DelCurr();
+	
+	//1 2 3 4 5
+	for (l.Revert(); !l.IsEnd(); l.GoNext())
+	{
+		cout << l.GetCurr() << ' ';
+	}
+	cout << "\n";
+
+	l.DelFirst();
+
+	//1 2 3 4 5
+	for (l.Revert(); !l.IsEnd(); l.GoNext())
+	{
+		cout << l.GetCurr() << ' ';
+	}
+	cout << "\n";
 }
--- mp2-lab5-list/TList.h
+++ mp2-lab5-list/TList.h
@@ -1,12 +1,40 @@
 #pragma once
 #include <iostream>
 
+/* TListException - класс исключения для TList */
+
+class TLException
+{
+private:
+	std::string desc;
+
+public:
+	TLException(std::string _desc)
+	{
+		desc = _desc;
+	}
+	TLException(const TLException& e)
+	{
+		desc = e.desc;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const TLException& e)
+	{
+		os << "Exception message: " << e.desc << '\n';
+		return os;
+	}
+};
+
+/* ...................... TNode ...................... */
+
 template <class T>
 struct TNode
 {
 	T value;
-	T* pNext;
+	TNode<T>* pNext;
 };
+
+/* ...................... TList ...................... */
 
 template <class T>
 class TList
@@ -14,7 +42,7 @@ class TList
 protected:
 	TNode<T>* pFirst;
 	TNode<T>* pLast;
-	//Для добавления эл-та в середину списка и удаления: [pPrev] -> [pCurr] -> []
+	//Для добавления эл-та в середину списка и удаления: [] -> [pPrev] -> [pCurr] -> []
 	TNode<T>* pPrev;
 	TNode<T>* pCurr;
 	//pStop будет равен указателю на голову, чтобы не иметь
@@ -24,120 +52,182 @@ protected:
 	int length;
 
 public:
-	TList()
-	{
-		pStop = nullptr;
-		pFirst = pLast = pPrev = pCurr = pStop;
-		length = 0;
-	}
+	TList();
+	~TList();
 
-	~TList()
-	{
-		while (pFirst != pStop)
-		{
-			TNode<T>* tmp = pFirst;
-			pFirst = pFirst->pNext;
-			delete tmp;
-		}
-		pLast = pPrev = pCurr = pStop;
-		length = 0;
-	}
-
-	bool IsEmpty()
-	{
-		return pFirst == pStop;
-	}
+	bool IsEmpty();
 
 	//Добавление элемента в начало списка
-	template <class T>
-	void InsFirst(T element)
-	{
-		TNode<T>* newNode = new TNode<T>();
-		newNode->value = element;
-		newNode->pNext = pFirst;
-		pFirst = newNode;
-		length++;
-		if (length == 1)
-			pLast = pFirst;
-	}
-
+	void InsFirst(T element);
 	//Добавление элемента в конец списка
-	template <class T>
-	void InsLast(T element)
-	{
-		if (length > 0)
-		{
-			TNode<T>* newNode = new TNode<T>();
-			newNode->value = element;
-			newNode->pNext = pStop;
-
-			pLast->pNext = newNode;
-			pLast = newNode;
-			length++;
-		}
-		else
-		{
-			InsFirst(element);
-		}
-	}
-
+	void InsLast(T element);
 	//Добавление элемента в текущую позицию (перед текущим) (текущим становится добавленный)
-	template <class T>
-	void InsCurr(T element)
-	{
-		if (pCurr == pFirst)
-			InsFirst(element);
-		else if (pPrev = pLast)
-			InsLast(element);
-		else
-		{
-			TNode<T>* newNode = new TNode<T>();
-			newNode->value = element;
-
-			newNode->pNext = pCurr;
-			pPrev->pNext = newNode;
-			pCurr = newNode;
-			length++;
-		}
-	}
+	void InsCurr(T element);
 
 	//Удаление первого элемента списка
-	template <class T>
-	void DelFirst()
-	{
-		if (length == 0) throw "Can't delete from an empty list";
+	void DelFirst();
+	//Удаление текущего элемента списка (текущим становится следующий за ним)
+	void DelCurr();
 
+	//Получение текущего элемента списка
+	T GetCurr();
+
+	//Возврат итератора в исходное состояние
+	void Revert();
+	//Перевод итератора далее
+	void GoNext();
+	//Проверка на достижение итератором конца списка
+	bool IsEnd();
+
+	friend std::ostream& operator<<(
+		std::ostream& out,
+		const TList<T>& l)
+	{
+		out << "[ ";
+		//TODO Later
+		out << "]";
+		return out;
+	}
+};
+
+template <class T>
+TList<T>::TList()
+{
+	pStop = nullptr;
+	pFirst = pLast = pPrev = pCurr = pStop;
+	length = 0;
+}
+
+template <class T>
+TList<T>::~TList()
+{
+	while (pFirst != pStop)
+	{
 		TNode<T>* tmp = pFirst;
 		pFirst = pFirst->pNext;
 		delete tmp;
-
-		length--;
-
-		if (length == 0)
-			pLast = pPrev = pCurr = pStop;
 	}
+	pLast = pPrev = pCurr = pStop;
+	length = 0;
+}
 
-	//Удаление текущего элемента списка (текущим становится следующий за ним)
-	template <class T>
-	void DelCurr()
+template <class T>
+bool TList<T>::IsEmpty()
+{
+	return pFirst == pStop;
+}
+
+template <class T>
+void TList<T>::InsFirst(T element)
+{
+	TNode<T>* newNode = new TNode<T>();
+	newNode->value = element;
+	newNode->pNext = pFirst;
+	pFirst = newNode;
+	length++;
+	if (length == 1)
+		pLast = pFirst;
+}
+
+template <class T>
+void TList<T>::InsLast(T element)
+{
+	if (length > 0)
 	{
-		if (length == 0) throw "Can't delete from an empty list";
+		TNode<T>* newNode = new TNode<T>();
+		newNode->value = element;
+		newNode->pNext = pStop;
 
-		/* !!! Не факт, что это работает */
-		if (pCurr == pFirst)
-		{
-			DelFirst();
-			pCurr = pFirst;
-		}
-		else
-		{
-			pPrev->pNext = pCurr->pNext;
-			delete pCurr;
-			pCurr = pPrev->pNext;
-			length--;
-		}
+		pLast->pNext = newNode;
+		pLast = newNode;
+		length++;
 	}
-};
+	else
+	{
+		InsFirst(element);
+	}
+}
+
+template <class T>
+void TList<T>::InsCurr(T element)
+{
+	if (pCurr == pFirst)
+		InsFirst(element);
+	else if (pPrev == pLast)
+		InsLast(element);
+	else
+	{
+		TNode<T>* newNode = new TNode<T>();
+		newNode->value = element;
+
+		newNode->pNext = pCurr;
+		pPrev->pNext = newNode;
+		pCurr = newNode;
+		length++;
+	}
+}
+
+template <class T>
+void TList<T>::DelFirst()
+{
+	if (pFirst == pStop)
+		throw TLException("Can't delete first element: it's a barrier");
+
+	TNode<T>* tmp = pFirst;
+	pFirst = pFirst->pNext;
+	delete tmp;
+	length--;
+}
+
+template <class T>
+void TList<T>::DelCurr()
+{
+	if (pCurr == pStop)
+		throw TLException("Can't delete current element: it's a barrier");
+
+	if (pFirst == pCurr)
+	{
+		DelFirst();
+	}
+	else
+	{
+		TNode<T>* tmp = pCurr;
+		pCurr = pCurr->pNext;
+		pPrev->pNext = pCurr;
+		delete tmp;
+		length--;
+	}
+}
+
+template <class T>
+T TList<T>::GetCurr()
+{
+	if (pCurr == pStop)
+		throw TLException("Can't get current element: it's a barrier");
+	return pCurr->value;
+}
+
+template <class T>
+void TList<T>::Revert()
+{
+	pPrev = pStop;
+	pCurr = pFirst;
+}
+
+template <class T>
+void TList<T>::GoNext()
+{
+	pPrev = pCurr;
+	pCurr = pCurr->pNext;
+}
+
+template <class T>
+bool TList<T>::IsEnd()
+{
+	return pCurr == pStop;
+}
+
+/* ..................... TMonom ..................... */
 
 struct TMonom
 {
